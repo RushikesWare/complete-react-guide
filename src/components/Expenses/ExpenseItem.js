17e4233628fab320686c80dd3008b1d41091c12f
Rushikesh Ware <2545994@tcs.com>
2023-01-31 10:31:52 +0530

    add use of useState and event Handler

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,13 +1,21 @@
 import ExpenseDate from './ExpenseDate';
 import Card from './Card';
 import './ExpenseItem.css';
+import React,{useState} from 'react';
 function ExpenseItem(props) {
+
+    const [title, setTitle]= useState(props.title);
+    const clickHandler =() =>{
+        setTitle("Updated..!");
+    }
+
     return (
       <Card className='expense-item'>
         <ExpenseDate date={props.date} />
         <div className='expense-item__description'>
-          <h2>{props.title}</h2>
+          <h2>{title}</h2>
           <div className='expense-item__price'>${props.amount}</div>
+          <button onClick={clickHandler}>change Title</button>
         </div>
       </Card>
     );
